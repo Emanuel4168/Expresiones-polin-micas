49fcbaa71b66aebe74d11230af5a34297fcf7dd4
Unknown <emanuel.l4168@gmail.com>
2018-10-31 16:03:48 -0700

    Creación de la clase nodo
    Se creó la clase Nodo, que tiene por objetivo unir un objeto y su nodo siguiente

---
+++ Expresiones polinomicas/src/expresionesPolinomicas/Nodo.java
@@ -0,0 +1,23 @@
+package expresionesPolinomicas;
+
+class Nodo<T>
+{
+	   public T Info;
+	   private Nodo<T> Sig;
+	   
+	   public Nodo(T D)
+	   {
+		  Info=D;
+	      Sig=null;
+	   }
+
+	   public Nodo<T> getSig()
+	   {
+		   return Sig;
+	   }
+
+	   public void setSig(Nodo<T> Ap)
+	   {
+		   Sig=Ap;
+	   }
+	}
