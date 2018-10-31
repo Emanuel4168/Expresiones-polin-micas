92361001a51996dd37499fd6deb7bf6a815f946f
Unknown <emanuel.l4168@gmail.com>
2018-10-31 16:36:21 -0700

    Creación de la clase Rutinas
    Se creó la clase rutinas, donde se almacenan métodos auxiliares

--- Expresiones polinomicas/src/expresionesPolinomicas/AplSumaPol.java
+++ Expresiones polinomicas/src/expresionesPolinomicas/AplSumaPol.java
@@ -1,6 +1,19 @@
 package expresionesPolinomicas;
 
-public class AplSumaPol {
+public class AplSumaPol 
+{
+	private static class Termino 
+	{
+		int base,potencia;
+		
+		public Termino(int base, int potencia)
+		{
+			this.base = base;
+			this.potencia = potencia;
+		}
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
